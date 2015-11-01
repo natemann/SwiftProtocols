7d581bf31ba4decddbb0d7539c53a6f7274917e1
Nate Mann <nathan.mann@me.com>
2015-10-31 20:25:07 -0500

    CurrencyStrubgConvertable Added

---
+++ CurrencyStringConvertable.swift
@@ -0,0 +1,33 @@
+//
+//  NSNumber Extensions.swift
+//  InTheBlack
+//
+//  Created by Nathan Mann on 9/2/15.
+//  Copyright © 2015 Nathan Mann. All rights reserved.
+//
+
+import Foundation
+
+public protocol CurrencyStringConvertable {
+    
+    func toCurrencyString() -> String
+    
+}
+
+
+extension NSNumber: CurrencyStringConvertable {
+    
+    public func toCurrencyString() -> String {
+        let formatter = NSNumberFormatter()
+        
+        formatter.numberStyle    = .CurrencyStyle
+        formatter.negativeFormat = "-¤#,##0.00"
+        return formatter.stringFromNumber(self)!
+    }
+    
+}
+
+
+
+
+
