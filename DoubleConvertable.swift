012813ef30866b4a6adac5d42213c6bed1fa85b0
Nate Mann <nathan.mann@me.com>
2015-10-31 20:24:55 -0500

    DoubleConvertable Added

--- DoubleConvertable.swift
+++ DoubleConvertable.swift
@@ -10,14 +10,28 @@ import Foundation
 
 
 protocol DoubleConvertable {
+    
     func toDouble() -> Double?
+    
 }
 
 
 extension String: DoubleConvertable {
     
     func toDouble() -> Double? {
-        return Double(self)
+        //        ordinary number string to double
+        if let double = NSNumberFormatter().numberFromString(self)?.doubleValue {
+            return double
+        }
+        else {
+            //            currency string to double
+            let formatter = NSNumberFormatter()
+            
+            formatter.numberStyle = .CurrencyStyle
+            formatter.negativeFormat = "-¤#,##0.00"
+            return formatter.numberFromString(self)?.doubleValue
+        }
     }
     
 }
+
